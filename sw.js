d6354e6d8118bb468b73e94bc62b2062454742bc
Zack Scholl <zack.scholl@gmail.com>
2019-05-15 12:23:29 -0700

    first commit

---
+++ sw.js
@@ -0,0 +1,36 @@
+const version = "0.0.1";
+const cacheName = `rwtxtoffline-${version}`;
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                    `/`,
+                    `/index.html`,
+                    `/style.css`,
+                    `/js/crypto-js.min.js`,
+                    `/js/localforage.js`,
+                    `/js/lunr.min.js`,
+                    `/js/lz-string.js`,
+                    `/js/pako.min.js`,
+                    `/js/showdown.min.js`,
+                ])
+                .then(() => self.skipWaiting());
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open(cacheName)
+        .then(cache => cache.match(event.request, {
+            ignoreSearch: true
+        }))
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
